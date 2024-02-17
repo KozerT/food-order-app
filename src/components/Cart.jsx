e709c497c86dfbddc611e52c1755ea382bd428bd
KozerT <kozer.vizastaff@gmail.com>
2024-02-17 23:40:53 +0100

    feat: added a an items increase and decrease functionality to the Cart Modal component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formating";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const cartContextObject = useContext(CartContext);
@@ -13,20 +14,30 @@ const Cart = () => {
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+
+  const handleCloseCart = () => {
+    userProgressCtx.hideCart();
+  };
+
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartContextObject.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            {...item}
+            onIncrease={() => cartContextObject.addItem(item)}
+            onDecrease={() => cartContextObject.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close </Button>
-        <Button>Go to Checkout </Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout </Button>
       </p>
     </Modal>
   );
